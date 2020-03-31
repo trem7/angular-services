08adfa13e5be4d7faad3089d9ff8a36fd3969fb2
Adam <adam.tremblett7@gmail.com>
2020-03-31 10:46:02 -0400

    initial commit

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { stat } from 'fs';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+
+  constructor(private loggingService: LoggingService) {}
+
+  onSetTo(status: string) {
+    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.loggingService.logStatusChange(status);
+  }
+}
