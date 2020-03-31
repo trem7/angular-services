5ce331d49d0b2e67594f51b050a9f0bf4bb72da5
Adam <adam.tremblett7@gmail.com>
2020-03-31 11:09:35 -0400

    creating a data service

---
+++ src/app/accounts.service.ts
@@ -0,0 +1,26 @@
+import { stringify } from "querystring";
+
+export class AccountsService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({name: name, status: status});
+  }
+
+  updatesStatus(id: number, status: string) {
+    this.accounts[id].status = status;
+  }
+}
